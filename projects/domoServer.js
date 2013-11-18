a706782d1a0d5cb71831b4266eeb426a85f1ab58
Geoffrey Vandiest <geoffrey@belgianagencies.com>
2013-11-18 22:01:45 +0100

    again refactoring

--- projects/domoServer.js
+++ projects/domoServer.js
@@ -5,9 +5,13 @@ var static = require('node-static')
 var assert = require("assert");
 
 var DomoServer = (function () {
-    function DomoServer(noduino) {
+    function DomoServer(noduino, fileServer) {
         this.noduino = noduino;
-        this.fileServer = new static.Server('./domotique/public');
+        this.fileServer = fileServer;
+    }
+
+    DomoServer.prototype.onConnect = function(connectHandler){
+        this.connect = connectHandler;
     }
 
     DomoServer.prototype.listen = function(){
@@ -20,22 +24,19 @@ var DomoServer = (function () {
         var app = http.createServer(handler);
         var io = socketio.listen(app);
         io.sockets.on('connection', function (socket) {
-            self.noduino.connect(function (err, board) {
-                var arduino = require('duino');
-                if(err) { return console.log(err); }
-                board.withAnalogInput({pin: 'A2'}, function (err, AnalogInput) {
-                    AnalogInput.on('change', function (a) {
-                        socket.emit('lightintensity', a.value);
-                    });
-                });
-            });
+            self.socket = socket;
+            self.noduino.connect(self.connect);
         });
+
         app.listen(8080);
         console.log('Server running at http://127.0.0.1:8080/');
     };
 
+
     return DomoServer;
 })();
+
+
 requirejs.config({
     nodeRequire: require
 });
@@ -44,5 +45,14 @@ requirejs.config({nodeRequire: require});
 requirejs(['../public/scripts/libs/Noduino', '../public/scripts/libs/Noduino.Serial', '../public/scripts/libs/Logger'], function (NoduinoObj, NoduinoConnector, Logger) {
 
     var server = new DomoServer(new NoduinoObj({'debug': false}, NoduinoConnector, Logger), new static.Server('./domotique/public'));
+    server.onConnect(function (err, board) {
+        if(err) { return console.log(err); }
+        board.withAnalogInput({pin: 'A2'}, function (err, AnalogInput) {
+            AnalogInput.on('change', function (a) {
+                server.socket.emit('lightintensity', a.value);
+            });
+        });
+    });
     server.listen();
+
 });
